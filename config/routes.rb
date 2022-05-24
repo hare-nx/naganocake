6ff6a95bc87f5f2161b72a203effa358bb7bca40
Akari Nakamura <103103248+hare-nx@users.noreply.github.com>
2022-05-24 11:01:07 +0000

    Update routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,60 +1,37 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'orders/show'
-  end
-  namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-  end
-  namespace :admin do
-    get 'genres/index'
-    get 'genres/edit'
-  end
-  namespace :admin do
-    get 'items/index'
-    get 'items/new'
-    get 'items/show'
-    get 'items/edit'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
-  namespace :public do
-    get 'addresses/index'
-    get 'addresses/edit'
-  end
-  namespace :public do
-    get 'orders/new'
-    get 'orders/complete'
-    get 'orders/index'
-    get 'orders/show'
-  end
-  namespace :public do
-    get 'cart_items/index'
-  end
-  namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/unsubscribe'
-  end
-  namespace :public do
-    get 'items/index'
-    get 'items/show'
-  end
-  
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
-  
+
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
+
+  namespace :admin do
+    root to: "homes#top"
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show, :update]
+  end
+
 
   devise_for :customers, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
+
+  namespace :public do
+    root to: "homes#top"
+    get "about" => "homes#about"
+    resources :items, only: [:index, :show]
+    resource :customers, only: [:show, :edit, :update]
+    get "customers/unsubscribe"
+    patch "customers/withdraw"
+    resources :cart_items, only: [:index, :update, :destroy, :create]
+    delete "cart_items/destroy_all"
+    resources :orders, only: [:new, :create, :index, :show]
+    post "orders/confirm"
+    get "orders/complete"
+    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
