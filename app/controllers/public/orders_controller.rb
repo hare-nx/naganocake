df670b7f74da8077bd3314dc8afb85db89881769
Akari Nakamura <103103248+hare-nx@users.noreply.github.com>
2022-05-29 09:39:07 +0000

    [Add] orders

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,5 +1,15 @@
 class Public::OrdersController < ApplicationController
   def new
+    @order=Order.new
+    @order.postal_code = current_customer.postal_code
+    @order.address = current_customer.address
+    @order.name = current_customer.last_name + current_customer.first_name
+  end
+
+  def create
+    @order=Order.new(order_params)
+    @order.save
+    redirect_to orders_confirm_path
   end
 
   def complete
@@ -10,4 +20,10 @@ class Public::OrdersController < ApplicationController
 
   def show
   end
+
+
+  private
+    def order_params
+      params.require(:order).permit(:payment, :postal_code, :address, :name)
+    end
 end
